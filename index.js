88d22a11c94122de0418de5424f3622f58bd7b80
Hubert Osypowicz <hubertosypowicz@outlook.com>
2020-07-21 00:55:39 +0200

    Start playing tracks

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import { AppRegistry } from "react-native";
 import App from "./App";
 import { name as appName } from "./app.json";
+import TrackPlayer from "react-native-track-player";
+TrackPlayer.registerPlaybackService(() =>
+  require("./src/services/TrackPlayerService.js"),
+);
 
 AppRegistry.registerComponent(appName, () => App);
--- src/Screen/MainScreen.js
+++ src/Screen/MainScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { View, StyleSheet, ScrollView, TouchableHighlight } from "react-native";
 import SearchInput from "../Components/searchInput";
 import GetMusicListApi from "../Web/getMusicList";
@@ -6,6 +6,9 @@ import ParseToVideoList from "../Web/parseToVideoList";
 import GetUrlApi from "../consts/GetYoutubeApiUrl";
 import ItemList from "../Components/ItemList";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import TrackPlayer from "react-native-track-player";
+import ytdl from "react-native-ytdl";
+
 import {
   faArrowLeft,
   faPause,
@@ -15,7 +18,7 @@ import {
 const MainScreen = () => {
   const [search, setSearch] = useState("");
   const [musicList, setMusicList] = useState([]);
-  const [videoUrl, setVideoUrl] = useState("");
+  const [trackList, setTrackList] = useState([]);
   console.log("test");
   const searchHandle = (text) => {
     setSearch(text);
@@ -26,9 +29,52 @@ const MainScreen = () => {
     setMusicList(videoList);
     console.log(musicList);
   };
-  const selectedMusic = (url) => {
-    console.log(url);
+  const selectedMusic = async (url, image, title, id) => {
+    const youtubeURL = url;
+    const urls = await ytdl(youtubeURL, { quality: "highestaudio" });
+    let state = await TrackPlayer.getState();
+    if (state == 3) {
+      await TrackPlayer.add([]);
+      await TrackPlayer.reset();
+    }
+    let track = {
+      id,
+      url: urls[0].url,
+      title,
+      artwork: image,
+    };
+    setTrackList([track]);
+    await TrackPlayer.add([track]);
+    await TrackPlayer.play();
   };
+
+  useEffect(() => {
+    const initi = async () => {
+      try {
+        await TrackPlayer.setupPlayer().then(() => {
+          console.log("Track player working");
+        });
+        await TrackPlayer.updateOptions({
+          // One of RATING_HEART, RATING_THUMBS_UP_DOWN, RATING_3_STARS, RATING_4_STARS, RATING_5_STARS, RATING_PERCENTAGE
+
+          // Whether the player should stop running when the app is closed on Android
+          stopWithApp: true,
+
+          // An array of media controls capabilities
+          // Can contain CAPABILITY_PLAY, CAPABILITY_PAUSE, CAPABILITY_STOP, CAPABILITY_SEEK_TO,
+          // CAPABILITY_SKIP_TO_NEXT, CAPABILITY_SKIP_TO_PREVIOUS, CAPABILITY_SET_RATING
+          capabilities: [
+            TrackPlayer.CAPABILITY_PLAY,
+            TrackPlayer.CAPABILITY_PAUSE,
+            TrackPlayer.CAPABILITY_STOP,
+          ],
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    initi();
+  }, []);
 
   return (
     <View style={styles.mainContainer}>
@@ -62,7 +108,12 @@ const MainScreen = () => {
               videoTitle={item.videoTitle}
               videoImage={item.videoImage}
               onPress={() => {
-                selectedMusic(item.videoUrl);
+                selectedMusic(
+                  item.videoUrl,
+                  item.videoImage,
+                  item.videoTitle,
+                  item.id,
+                );
               }}
             />
           );
